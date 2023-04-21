ea42aa79fbf854d806f84588504349380b2acb36
Rachael McIlhagga <rmcilhagga@tvo.org>
2023-04-20 21:38:27 -0400

    adding block location storage and indication of overlap in position

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,42 @@ import { Canvas } from '@react-three/fiber'
 import Box from './Box'
 import { Stats, OrbitControls } from '@react-three/drei'
 import useKeyboard from './usekeyboard'
+import { useState } from 'react'
 
 export default function App() {
   const keyMap = useKeyboard()
   const pos = [-1.5, 5.5, 0.5]
+  const [posStore, setPostStore] = useState([])
+
+  function checkExistance(newPosition) {
+    if (posStore.find(item => item.x === newPosition.x && item.y === newPosition.y && item.y === newPosition.y)) {
+      console.log('match found')
+      return true;
+    }
+  }
+
+  const updateState = (newState) => {
+    let objPos = { x: newState.x, y: newState.y, z: newState.z };
+
+    console.log(newState)
+    if (checkExistance(objPos)) {
+      console.log('stop movement')
+    } else {
+      console.log('store it')
+      setPostStore((prevState) => [...prevState, objPos]);
+    }
+  };
+
   return (
-    <Canvas camera={{ position: [1, 6, 8] }} >
+    <Canvas camera={{ position: [3, 6, 8] }} >
       {[...Array(4)].map((x, index) => (
-        <Box position={[pos[0] + index, pos[1], pos[2]]} key={index} keyMap={keyMap} />
+        <Box position={[pos[0] + index, pos[1], pos[2]]} updateState={updateState} key={index} keyMap={keyMap} />
       ))}
       <OrbitControls maxPolarAngle={Math.PI / 2} />
       <axesHelper args={[5]} />
       <gridHelper />
       <Stats />
+
     </Canvas>
   )
 }
--- src/Box.jsx
+++ src/Box.jsx
@@ -3,15 +3,16 @@ import { useFrame } from '@react-three/fiber'
 
 export default function Box(props) {
   const ref = useRef()
-  
+
   const [selected, setSelected] = useState()
   const [movingLeft, setMovingLeft] = useState(false)
   const [movingRight, setMovingRight] = useState(false)
   const [movingDown, setMovingDown] = useState(false)
-  const [locked , setIsLocked] = useState(false)
-
+  const [locked, setIsLocked] = useState(false)
+  console.log(props)
   useFrame((_, delta) => {
-    if(ref.current.position.y === 0.5){
+    // If its sitting at the bottom of the board, restrict movement
+    if (ref.current.position.y === 0.5) {
       setIsLocked(true)
     }
     // If space bar pressed, send all the way down.
@@ -21,27 +22,34 @@ export default function Box(props) {
     if (props.keyMap['KeyA'] && selected && !movingLeft && ref.current.position.x > -4.5 && !locked) {
       ref.current.position.x -= 1
       setMovingLeft(true)
+      props.updateState(ref.current.position);
+
     } else if (!props.keyMap['KeyA'] && movingLeft) {
       setMovingLeft(false)
     }
     if (props.keyMap['KeyD'] && selected && !movingRight && ref.current.position.x < 4.5 && !locked) {
       ref.current.position.x += 1
       setMovingRight(true)
+      props.updateState(ref.current.position);
+
     } else if ((!props.keyMap['KeyD'] && movingRight)) {
       setMovingRight(false)
     }
     if (props.keyMap['KeyS'] && selected && !movingDown && ref.current.position.y > 0.5 && !locked) {
       ref.current.position.y -= 1
       setMovingDown(true)
+      props.updateState(ref.current.position);
+      // props.updatePosStore(ref.current.position)
     } else if ((!props.keyMap['KeyS'] && movingDown)) {
       setMovingDown(false)
     }
-  })
 
+  })
   return (
     <mesh ref={ref} {...props} onPointerDown={() => setSelected(!selected)}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe={!selected} />
     </mesh>
+
   )
 }
